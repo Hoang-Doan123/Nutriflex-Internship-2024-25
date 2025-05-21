9e86d177f65c33ec85196c670a33dff030d083ca
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2025-05-21 15:30:35 +0700

    Add new app files

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Nutriflex-Internship-2024-25"
+include(":app")
+ 
