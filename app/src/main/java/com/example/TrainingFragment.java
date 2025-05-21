70fd01a7d8731ea1c2d27d95ec85ac97074dff70
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2025-05-21 17:38:41 +0700

    Add modules' names

--- app/src/main/java/com/example/TrainingFragment.java
+++ app/src/main/java/com/example/TrainingFragment.java
@@ -59,6 +59,8 @@ public class TrainingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_training, container, false);
+        View view = inflater.inflate(R.layout.fragment_training, container, false);
+
+        return view;
     }
 }
