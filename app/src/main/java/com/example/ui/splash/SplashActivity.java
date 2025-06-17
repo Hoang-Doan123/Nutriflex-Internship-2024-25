edd9727824ef080245d931d1708d52b25ca21dc6
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2025-06-17 12:59:42 +0700

    Create splash package

---
+++ app/src/main/java/com/example/ui/splash/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.ui.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.R;
+import com.example.ui.onboarding.OnboardingActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, OnboardingActivity.class));
+            finish();
+        }, 2000); // 2 seconds delay
+    }
+}
